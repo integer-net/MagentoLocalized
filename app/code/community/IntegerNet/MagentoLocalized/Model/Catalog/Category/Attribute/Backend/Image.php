9d271e0623342ad6a1974205a6606968d0055d51
Andreas von Studnitz <avs@integer-net.de>
2013-11-05 17:06:48 +0100

    Removed exception log entries when saving categories

---
+++ app/code/community/IntegerNet/MagentoLocalized/Model/Catalog/Category/Attribute/Backend/Image.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * integer_net Magento Module
+ *
+ * @category   IntegerNet
+ * @package    IntegerNet_MagentoLocalized
+ * @copyright  Copyright (c) 2013 integer_net GmbH (http://www.integer-net.de/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author     Andreas von Studnitz <avs@integer-net.de>
+ */ 
+class IntegerNet_MagentoLocalized_Model_Catalog_Category_Attribute_Backend_Image extends Mage_Catalog_Model_Category_Attribute_Backend_Image
+{
+    /**
+     * Save uploaded file and set its name to category
+     *
+     * @param Varien_Object $object
+     * @return void
+     */
+    public function afterSave($object)
+    {
+        $value = $object->getData($this->getAttribute()->getName());
+
+        // if no image was set - nothing to do
+        if (empty($value) && empty($_FILES)) {
+            return;
+        }
+
+        if (is_array($value) && !empty($value['delete'])) {
+            $object->setData($this->getAttribute()->getName(), '');
+            $this->getAttribute()->getEntity()
+                ->saveAttribute($object, $this->getAttribute()->getName());
+            return;
+        }
+
+        $path = Mage::getBaseDir('media') . DS . 'catalog' . DS . 'category' . DS;
+
+        try {
+            $uploader = new Mage_Core_Model_File_Uploader($this->getAttribute()->getName());
+            $uploader->setAllowedExtensions(array('jpg','jpeg','gif','png'));
+            $uploader->setAllowRenameFiles(true);
+            $result = $uploader->save($path);
+
+            $object->setData($this->getAttribute()->getName(), $result['file']);
+            $this->getAttribute()->getEntity()->saveAttribute($object, $this->getAttribute()->getName());
+        } catch (Exception $e) {
+            if ($e->getCode() != Mage_Core_Model_File_Uploader::TMP_NAME_EMPTY) {
+                Mage::logException($e);
+            }
+            /** @TODO ??? */
+            return;
+        }
+    }
+
+}
